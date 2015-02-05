b774208eccafbef7c9af27db44f73423fd5ede73
diablogatox <diablogatox@gmail.com>
2015-02-05 19:08:20 +0800

    test

--- src/com/orfid/internetcommunity/RightProvision.java
+++ src/com/orfid/internetcommunity/RightProvision.java
@@ -3,11 +3,15 @@ package com.orfid.internetcommunity;
 import android.app.Activity;
 import android.os.Bundle;
 import android.text.Html;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class RightProvision extends Activity {
 
 	TextView textView;
+	ImageView backBtn;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +19,16 @@ public class RightProvision extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.right_provision);
 		textView = (TextView) findViewById(R.id.main_content);
+		backBtn = (ImageView) findViewById(R.id.back);
+		backBtn.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				finish();
+				
+			}
+			
+		});
 		
 		textView.setText(Html.fromHtml(getResources().getString(R.string.right_provision)));
 	}
